abf5788228464e52b2b19ca70882c2a58d87cbc0
Chefor Sylvanus <cheforsylvanus@gmail.com>
2026-09-09 20:00:34 +0100

    Create and test update, create and delete category apis

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./api/auth.js";
+import categoryRoutes from "./api/categories.js";
 import connectDB from "./config/db.js";
 
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
 connectDB();
 
 
