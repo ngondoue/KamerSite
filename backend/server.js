9c432e9573585cab9a44c417d8749b548cccaaac
Faith Bervely <ngondoue.bervel@ictuniversity.edu.cm>
2026-09-10 15:03:41 +0200

    little update

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
 connectDB();
+
+app.use("/api/auth", authRoutes);
 
 
 app.listen(PORT, () => {
