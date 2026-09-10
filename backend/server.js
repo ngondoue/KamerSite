922d5edf93f3035875f14f0222c6120f4d9713b9
Chefor Sylvanus <cheforsylvanus@gmail.com>
2026-09-10 21:06:31 +0100

    Create get one place api, get all published places api and get nearby places api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./api/auth.js";
 import categoryRoutes from "./api/categories.js";
+import placesRoutes from "./api/places.js"
 import connectDB from "./config/db.js";
 
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/places", placesRoutes)
 connectDB();
 
 
